88c6bdd2e46e940677aa4c6a95e830bc0a204019
ievas <ieva.ritina@gmail.com>
2024-02-07 22:10:24 -0700

    extra routes: random color

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -6,16 +6,21 @@ export default function Random() {
     let [color, setColor] = useState("#000000")
 
     function randomColor(){
-        //generate a random color
-        //how to?
+
+        let randomColor = Math.floor(Math.random()*16777215).toString(16); //from https://css-tricks.com/snippets/javascript/random-hex-color/;
+
+        setColor(`#${randomColor}`);
     }
 
     useEffect(() => {
-        //make a call to the color api with that randomly generated color
-    })
+        randomColor();
+        
+    }, [])
+
+    let backgroundColor = color !== "#000000" ? {backgroundColor: color } : {};
 
     return (
-        <div id="random-color-container">
+        <div id="random-color-container" style={backgroundColor}>
             <h1>{color}</h1>
         </div>
     )
